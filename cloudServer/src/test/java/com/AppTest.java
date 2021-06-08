289806ee7d6fdcb14734c7ad1dfe9482fd230b3d
cmqMercer <cmq_123321@163.com>
2021-06-08 16:46:58 +0800

    join Druid

--- cloudServer/src/test/java/com/AppTest.java
+++ cloudServer/src/test/java/com/AppTest.java
@@ -3,6 +3,8 @@ package com;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.core.SpringVersion;
 
 /**
  * Unit test for simple App.
@@ -12,9 +14,24 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+
+    public void getSpringVersion() {
+        String version = SpringVersion.getVersion();
+
+        String version1 = SpringBootVersion.getVersion();
+
+        System.out.println(version);
+
+        System.out.println(version1);
+
     }
+
 }
+
