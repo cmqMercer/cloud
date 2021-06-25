d7524fc1fea64c376d2defbdbb998271e549c9dd
cmqMercer <cmq_123321@163.com>
2021-06-25 15:53:08 +0800

    GlobalExceptionHandler

--- cloudServices/src/main/java/com/controller/Controller.java
+++ cloudServices/src/main/java/com/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.controller;
 
+import com.common.BizException;
 import com.entity.User;
 import com.services.UserSevices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,4 +27,30 @@ public class Controller {
     return "FID:"+user.getFID()+"  FPASSWORD:"+user.getFPASSWORD();
     }
 
+    @RequestMapping("/bizExe")
+    public boolean bizExe(@RequestParam String name) {
+
+        //如果姓名为空就手动抛出一个自定义的异常！
+        if(name.equals("aa")){
+            throw  new BizException("-1","用户姓名不能为空！");
+        }
+        return true;
+    }
+
+    @RequestMapping("/bizExeNull")
+    public boolean bizExeNull(@RequestParam String name) {
+
+        //如果姓名为空就手动抛出一个自定义的异常！
+        String a = null;
+         a.equals("aa") ;
+
+        return true;
+    }
+    @RequestMapping("/bizExeTrans")
+    public boolean bizExeTrans(@RequestParam String name) {
+
+        //如果姓名为空就手动抛出一个自定义的异常！
+         Integer.parseInt(name);
+        return true;
+    }
 }
